a5470878356b1322e994da85e7cab3daafdd224f
AnkitSharma <sssonusharma1999@gmail.com>
2021-03-13 10:52:27 +0530

    ml commit

---
+++ practices.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[25]:
+
+
+def add():
+    print('add')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
